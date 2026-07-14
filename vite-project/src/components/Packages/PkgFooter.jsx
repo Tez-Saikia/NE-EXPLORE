5b2f018ffe9b676e2871a6f6cef15c1747925af8
Tez-Saikia <tezsaikia14@gmail.com>
2026-07-14 13:21:22 +0530

    logo.png error

--- vite-project/src/components/Packages/PkgFooter.jsx
+++ vite-project/src/components/Packages/PkgFooter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-import Logo from "../../assets/Logo.png";
+import Logo from "@/assets/logo.png";
 import { Link } from "react-router-dom";
 
 function PkgFooter() {
